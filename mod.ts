befa1871b7eadc4db85e41c96abbf7aa50803689
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-09-23 15:40:54 -0400

    feat: initial commit

---
+++ mod.ts
@@ -0,0 +1,3 @@
+// Exports for Standards. Please refer to child modules.
+export { Log } from './lib/module/log/log.std.ts';
+export { TransporterSeverity } from './lib/module/log/util/transporter.ts';
