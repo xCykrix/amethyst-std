72cf7a2acea761998d6daadca6f1500a10c317e9
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-09-27 14:07:57 -0400

    feat: add documentation to symbols

--- std/logging/logging.mod.ts
+++ std/logging/logging.mod.ts
@@ -1,22 +1,38 @@
 import { ConsoleHandler, getLogger, type LevelName, type LogConfig, type Logger, type LoggerConfig, RotatingFileHandler, setup } from '@std/log';
 import { cli, json } from './lib/formatter.ts';
 
+// Types
 type LoggerVariant = 'frontend' | 'backend' | 'cli' | 'fs';
 type LoggerHandlers = 'json' | 'fs';
 
+// Interface
 interface LoggerVariantConfig extends LoggerConfig {
   handlers: LoggerHandlers[];
 }
 
+/**
+ * logging.mod.ts Logging Standard API
+ */
 export class Logging {
+  /** Toggle for Console Logger.
+   * @internal */
   private createdConsoleLogger: boolean = false;
+  /** Toggle for File Logger
+   * @internal */
   private createdFileLogger: boolean = false;
-
+  /** Controls the State
+   * @internal */
   private state: LogConfig = {
     handlers: {},
     loggers: {},
   };
 
+  /**
+   * Create the Console Logger(s).
+   *
+   * @param level The {@link LevelName} to filter below. Use 'DEBUG' for all.
+   * @returns Logging
+   */
   public createConsoleLogger(level: LevelName): Logging {
     this.state.handlers!['cli'] = new ConsoleHandler(level, {
       formatter: cli,
@@ -30,6 +46,12 @@ export class Logging {
     return this;
   }
 
+  /**
+   * Create the File Logger(s).
+   *
+   * @param level The {@link LevelName} to filter below. Use 'DEBUG' for all.
+   * @returns Logging
+   */
   public createFileLogger(level: LevelName): Logging {
     this.state.handlers!['fs'] = new RotatingFileHandler(level, {
       formatter: json,
@@ -43,6 +65,11 @@ export class Logging {
     return this;
   }
 
+  /**
+   * Create the Default Logger Variants of 'frontend' | 'backend' | 'cli' | 'fs'.
+   *
+   * @returns Logging
+   */
   public createDefaultLoggerVariants(): Logging {
     this.createLoggerVariant('frontend', {
       level: 'DEBUG',
@@ -63,7 +90,15 @@ export class Logging {
     return this;
   }
 
-  public createLoggerVariant(id: LoggerVariant, options: LoggerVariantConfig): Logging {
+  /**
+   * Create a LoggerVariant.
+   *
+   * @param id The {@link LoggerVarient} Identifier.
+   * @param options The {@link LoggerVariantConfig}..
+   * @returns Logging
+   * @internal
+   */
+  private createLoggerVariant(id: LoggerVariant, options: LoggerVariantConfig): Logging {
     this.state.loggers![id] = {
       level: options.level ?? 'DEBUG',
       handlers: [...options.handlers ?? []],
@@ -71,11 +106,22 @@ export class Logging {
     return this;
   }
 
+  /**
+   * Configures {@link https://jsr.io/@std/log}
+   *
+   * @returns
+   */
   public build(): Logging {
     setup(this.state);
     return this;
   }
 
+  /**
+   * Fetch a {@link Logger} instance from @std/log.
+   *
+   * @param id The {@link LoggerVariant} to fetch.
+   * @returns A Logger.
+   */
   public getLogger(id: LoggerVariant = 'cli'): Logger {
     return getLogger(id);
   }
